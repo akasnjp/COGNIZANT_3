952d01fbee4a384daeee536889da37c08ae956eb
akasnjp <akasnjp@gmail.com>
2024-07-29 20:39:14 +0530

    third commit

--- Week_1/SingletonPatternExample/src/Logger.java
+++ Week_1/SingletonPatternExample/src/Logger.java
@@ -5,4 +5,33 @@ public class Logger {
 
     }
     
+    public static Logger getInstance(){
+        if(instance == null){
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void log(String message){
+        System.out.println("Log message: " + message);
+    }
+
+    public class LoggerTest{
+        public static void main(String[] args) {
+            Logger logger1 = Logger.getInstance();
+            Logger logger2 = Logger.getInstance();
+
+            logger1.log("This is the first log message");
+            
+            logger2.log("This is the second");
+
+            if(logger1 == logger2){
+                System.out.println("Both logger1 and logger2 refers to same instance");
+            }
+            else{
+                System.out.println("Not from same instance");
+            }
+        }
+       
+    }
 }
