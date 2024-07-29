22b1bc4cba8f1ad52b9b447b3ebd17f86f49de3e
akasnjp <akasnjp@gmail.com>
2024-07-29 22:16:32 +0530

    sixth commit

---
+++ Week_1/AdapterPatternExample/src/AdapterPatternExample.java
@@ -0,0 +1,13 @@
+public class AdapterPatternExample {
+    public static void main(String[] args) {
+        PayPalGateway payPalGateway = new PayPalGateway();
+        StripeGateway stripeGateway = new StripeGateway();
+
+        PaymentProcessor payPalAdapter = new PayPalAdapter(payPalGateway);
+        PaymentProcessor stripeAdapter = new StripeAdapter(stripeGateway);
+
+        payPalAdapter.processPayment(100.0);
+        stripeAdapter.processPayment(200.0);
+
+    }
+}
