21cc2d6f218f9b33b51898e64b33c5a0ebf4f436
akasnjp <akasnjp@gmail.com>
2024-07-30 13:44:05 +0530

    fourth commit

---
+++ Week_1/TaskManagementSystem/src/TaskLinkedList.java
@@ -0,0 +1,61 @@
+public class TaskLinkedList {
+    private Node head;
+
+    public TaskLinkedList() {
+        this.head = null;
+    }
+
+    public void addTask(Task task) {
+        Node newNode = new Node(task);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public Task searchTask(String taskId) {
+        Node current = head;
+        while (current != null) {
+            if (current.task.getTaskId().equals(taskId)) {
+                return current.task;
+            }
+            current = current.next;
+        }
+        return null;
+    }
+
+    public void traverseTasks() {
+        Node current = head;
+        while (current != null) {
+            System.out.println(current.task);
+            current = current.next;
+        }
+    }
+
+    public void deleteTask(String taskId) {
+        if (head == null) {
+            return;
+        }
+
+        if (head.task.getTaskId().equals(taskId)) {
+            head = head.next;
+            return;
+        }
+
+        Node current = head;
+        Node previous = null;
+        while (current != null && !current.task.getTaskId().equals(taskId)) {
+            previous = current;
+            current = current.next;
+        }
+
+        if (current != null) {
+            previous.next = current.next;
+        }
+    }
+}
