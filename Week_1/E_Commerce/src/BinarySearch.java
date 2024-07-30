2bf7633085bf0baafbe6109e716196ec288400e5
akasnjp <akasnjp@gmail.com>
2024-07-30 13:04:14 +0530

    commit

---
+++ Week_1/E_Commerce/src/BinarySearch.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+public class BinarySearch {
+    public static Product binarySearch(Product[] products, String targetProductId) {
+        int left = 0;
+        int right = products.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int comparison = products[mid].getProductId().compareTo(targetProductId);
+            if (comparison == 0) {
+                return products[mid];
+            }
+            if (comparison < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return null;
+    }
+
+    
+    public static void sortProducts(Product[] products) {
+        Arrays.sort(products, (p1, p2) -> p1.getProductId().compareTo(p2.getProductId()));
+    }
+
+}
