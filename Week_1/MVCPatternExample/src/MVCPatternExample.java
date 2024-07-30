8fe397292d65a4bb5f69d645720d41916e9ff9b1
akasnjp <akasnjp@gmail.com>
2024-07-30 11:39:28 +0530

    twelveth commit

---
+++ Week_1/MVCPatternExample/src/MVCPatternExample.java
@@ -0,0 +1,17 @@
+public class MVCPatternExample {
+    public static void main(String[] args) {
+        
+        Student student = new Student("1", "John Doe", "A");
+
+        StudentView view = new StudentView();
+ 
+        StudentController controller = new StudentController(student, view);
+
+        controller.updateView();
+
+        controller.setStudentName("Jane Doe");
+        controller.setStudentGrade("B");
+
+        controller.updateView();
+    } 
+}
