1a675128a8b99f65447c76b44e0036dc4ca7d5bb
akasnjp <akasnjp@gmail.com>
2024-08-06 08:21:52 +0530

    gg

---
+++ Week_2/Exercise_7/demo/src/main/java/library/BookRepository.java
@@ -0,0 +1,8 @@
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepository {
+    public BookRepository(){
+        System.out.println("Book Repository bean created !!! ");
+    }
+}
---
+++ Week_2/Exercise_7/demo/src/main/java/library/Main.java
@@ -0,0 +1,9 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class Main {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookService service=context.getBean(BookService.class);
+        service.displayBookRepository();
+        context.close();
+    }
+}
