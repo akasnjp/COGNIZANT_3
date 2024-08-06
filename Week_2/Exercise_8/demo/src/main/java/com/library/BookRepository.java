3485fb6b303ebc5744cc96e109400d1c9429bd06
akasnjp <akasnjp@gmail.com>
2024-08-06 08:28:42 +0530

    comit

---
+++ Week_2/Exercise_8/demo/src/main/java/com/library/BookRepository.java
@@ -0,0 +1,6 @@
+import org.springframework.stereotype.Repository;
+public class BookRepository {
+    public BookRepository(){
+        System.out.println("Book Repository bean created !!! ");
+    }
+}
